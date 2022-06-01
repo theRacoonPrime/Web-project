c4f4823d5fc139e10c4f58986b8a8627f609da94
theRacoonPrime <stoletov314@gmail.com>
2022-06-01 13:36:46 +0200

    Bootstrapp, likes, back button etc

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from . import views  # What is my problem ?
+from .views import HomeView, ArticleDetailView
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('blogpost-like/<int:pk>', views.BlogPostLike, name="blogpost_like"),
+    path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article_details'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,82 +1,33 @@
-from django.utils import timezone
-from django.views.generic import DetailView
-
-from .models import Post, BlogPost
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from .forms import PostForm
-from django.shortcuts import redirect
-
-# Like def and class
+from django.views.generic import ListView, DetailView
+from .models import Post
+from django.shortcuts import render
 
 
-def BlogPostLike(request, pk):
-    post = get_object_or_404(BlogPost, id=request.POST.get('blogpost_id'))
-    if post.likes.filter(id=request.user.id).exists():
-        post.likes.remove(request.user)
-    else:
-        post.likes.add(request.user)
-
-    return HttpResponseRedirect(reverse('blogpost-detail', args=[str(pk)]))
+def home(request):
+    return render(request, 'home.html', {})
 
 
-class BlogPostDetailView(DetailView):
-    model = BlogPost
-
-    def get_context_data(self, **kwargs):
-        data = super().get_context_data(**kwargs)
-        likes_connected = get_object_or_404(BlogPost, id=self.kwargs['pk'])
-        liked = False
-        if likes_connected.likes.filter(id=self.request.user.id).exists():
-            liked = True
-        data['number_of_likes'] = likes_connected.number_of_likes()
-        data['post_is_liked'] = liked
-        return data
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
 
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')  # No documentation found
-    return render(request, 'blog/post_list.html', {'posts': posts})
-
-# Post detail ( just for info)
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
 
 
-# New function (post new)
 
 
-def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
 
 
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+
+
+
 
 
 
