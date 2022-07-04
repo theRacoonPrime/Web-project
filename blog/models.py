c11e374fede93c01526efead7ef29beaf3d8c153
Andrey Stoletov <stoletov314@gmail.com>
2022-07-04 04:43:21 +0200

    new classes

--- blog/models.py
+++ blog/models.py
@@ -5,27 +5,26 @@ from django.urls import reverse
 from datetime import datetime, date
 
 
+class Categories(models.Model):
+    name_category = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name_category
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    title_tag = models.CharField(max_length=255, default=" My awesome blog")
+    title = models.CharField(max_length=200)
+    title_tag = models.CharField(max_length=200, default=" My awesome blog")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-
+    categories = models.CharField(max_length=200, default='uncategorized')
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
     def get_absolute_url(self):
         return reverse('home')
-
-    def total_likes(self):
-        return self.likes.count()
-
-
-class Category(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='category')
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    text = models.TextField()
 
