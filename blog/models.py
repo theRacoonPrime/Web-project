23c80cec8f446a07c816d6df9af9d0dfed5edfb0
Andrey Stoletov <stoletov314@gmail.com>
2022-09-11 13:38:52 +0200

    new classes

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils import timezone
+from django.contrib.auth.models import User
 from django.urls import reverse
-from datetime import datetime, date
 
 
-class Categories(models.Model):
-    name_category = models.CharField(max_length=200)
+class Comments(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name_category
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
     def get_absolute_url(self):
         return reverse('home')
@@ -17,13 +29,12 @@ class Categories(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    title_tag = models.CharField(max_length=200, default=" My awesome blog")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
-    categories = models.ManyToManyField(Categories)
+    categories = models.ManyToManyField(Category)
     created_date = models.DateTimeField(default=timezone.now)
     postdate = models.DateField(auto_now_add=True)
-    likes = models.ManyToManyField(User, related_name='blogpost_like')
+
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
